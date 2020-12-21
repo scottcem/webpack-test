a874350b9b64a393dcaf1409939a406fcca2432e
scottcem <74527234+scottcem@users.noreply.github.com>
2020-12-21 15:21:52 -0600

    first commit
    added app folder
    added dish folder

---
+++ app/javascript/another.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    let header = document.querySelector('#greeting');
+    let hello = ('<h1> hello world </h1>');
+    header.innerHTML = hello;
+}
+
+module.exports = sayHello();
---
+++ app/javascript/index.js
@@ -0,0 +1,9 @@
+//function sayHello() {
+   //let header = document.querySelector('#greeting');
+    //let hello = ('<h1> hello world </h1>');
+    //header.innerHTML = hello;
+//}
+
+//sayHello();
+require('../css/app.css');
+require("./another.js")
